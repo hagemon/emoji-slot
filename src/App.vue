43b16f87ceae6a883a5c60482276103408ec4210
hagemon <hagemon>
2022-01-25 20:33:55 +0800

    add emojis

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,24 @@
 <template>
   <div id="app">
-    <router-view/>
+    <router-view />
   </div>
 </template>
 
 <script>
-
 export default {
-  name: 'App'
-}
+  name: "App",
+};
 </script>
 
 <style>
 #app {
-background-color: #FCFAF2;;
-font-family: 'Arial';
+  background-color: #fcfaf2;
+  font-family: "Arial";
 }
-html, body {
+html,
+body {
   margin: 0;
   padding: 0;
-  background-color: #FCFAF2;;
+  background-color: #fcfaf2;
 }
-
 </style>
---
+++ src/components/Test.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="wrapper">
+    <div class="machine">
+      <div class="machine-header"></div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Test",
+  data() {
+    return {};
+  },
+  components: {},
+  methods: {},
+  mounted() {},
+};
+</script>
+
+<style lang="scss">
+$red: #eb706b;
+
+.machine {
+  width: 80vw;
+  height: 50vw;
+  border: 8px solid black;
+  border-radius: 35px;
+  max-width: 540px;
+  max-height: 370px;
+  background-color: white;
+}
+
+.machine-header {
+  width: 100%;
+  height: 50%;
+  background-color: $red;
+  border-radius: 28px 28px 15px 15px;
+  z-index: -1;
+  max-height: 160px;
+  max-width: 530px;
+}
+</style>
